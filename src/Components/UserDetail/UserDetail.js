ffa3a5ba24a2ecff2c0c9229d968823efe05a6fd
aboudaman <adaman2000@gmail.com>
2022-06-02 13:49:32 +0200

    updated from class to function based component

--- src/Components/UserDetail/UserDetail.js
+++ src/Components/UserDetail/UserDetail.js
@@ -1,32 +1,24 @@
-import {Component} from 'react'
 
-class UserDetail extends Component {
-  constructor(props) {
-    super();
-  }
+function UserDetail(props) {
+  const {firstName, lastName, picture, location} = props.userDetail
+  return (
+    <>
+    <h4>
+      {firstName} {lastName}
+    </h4>
+    <div>
+      <img src={picture} alt="User" width="150px" />
+    </div>
 
-  render() {
-    //   console.log(this.props)
-      const {firstName, lastName, picture, location} = this.props.userDetail
-    //   console.log(location)
-    return (
-      <>
-        <h4>
-          {firstName} {lastName}
-        </h4>
-        <div>
-          <img src={picture} alt="User" width="150px" />
-        </div>
+    <h6>Address</h6>
+    <p>City: {location.city}</p>
+    <p>Country: {location.country}</p>
+    <p>State: {location.state}</p>
+    <p>Street: {location.street}</p>
+    <p>Timezone: {location.timezone}</p>
+  </>
 
-        <h6>Address</h6>
-        <p>City: {location.city}</p>
-        <p>Country: {location.country}</p>
-        <p>State: {location.state}</p>
-        <p>Street: {location.street}</p>
-        <p>Timezone: {location.timezone}</p>
-      </>
-    );
-  }
+  )
 }
 
 export default UserDetail;
